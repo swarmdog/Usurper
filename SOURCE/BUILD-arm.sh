5f81878249e65745cb064bff572651d35af26ed1
swarmdog <47836485+swarmdog@users.noreply.github.com>
2019-04-16 14:52:20 -0500

    Update BUILD-arm.sh

--- SOURCE/BUILD-arm.sh
+++ SOURCE/BUILD-arm.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+# On Raspbian - Install pre-reqs: sudo apt-get -y install fpc fp-units-fv 
+# Fetch https://github.com/rickparrish/RMDoor to a directory side by side with Usurper
 set -e
 
 fpc -B -Tlinux -Parm -Mtp -Scgi -CX -Cg -O3 -g -gl -Xs -XX -l -vewnhibq \
